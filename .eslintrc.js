4c891f66510497d7ef3c7bf3a4408ebd7637a627
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-08-06 21:02:47 +0900

    add コメントを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
+/**
+ * eslint-config-standard
+ * "quotes": ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": false }]
+ */
+
 module.exports = {
   root: true,
   env: {
@@ -8,6 +13,18 @@ module.exports = {
     "@nuxtjs/eslint-config-typescript"
   ],
   rules: {
+    // X = index.vue の '"created" is called'
+
+    // "quotes": "warn"
+    // => 適用されるルールは ["warn", "single", { "avoidEscape": true, "allowTemplateLiterals": false }]
+    // Xは単一引用符で囲まれているので警告は発生しない
+
+    // "quotes": ["error", "double"]
+    // => 適用されるルールは ["error", "double"]
+    // Xは二重引用符で囲まれておらず、かつavoidEscape=falseなのでエラーが発生する
+
     "quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": false }]
+    // => 適用されるルールは ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": false }]
+    // Xは二重引用符で囲まれていないが、avoidEscape=trueなのでエラーは発生しない
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@ import Vue from "vue"
 
 export default Vue.extend({
   created () {
-    console.log("created")
+    console.log('"created" is called')
   }
 })
 </script>
